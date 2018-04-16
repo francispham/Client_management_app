2af9a5c3dc6ecb9f9c12aaefc0d4ef63000cfc79
Francis <francispham89@gmail.com>
2018-04-16 11:17:29 -0700

    working on Authentication

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,26 @@
 class Client < ApplicationRecord
+  belongs_to :users
+
+  before_validation :set_view_count
+
+  validates :first_name, :last_name, :address, :postal_code, presence: true
+
+  validates :phone_number, :age, presence: true, numericality: { only_integer: true }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            format: VALID_EMAIL_REGEX,
+            unless: :from_oauth?
+
+  include FriendlyId
+  friendly_id :full_name, use: [:slugged, :history, :finders]
+
+  def full_name
+    "#{first_name} #{last_name}".strip
+
+  end
+
 end
