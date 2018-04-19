49ecaebe6cbd8fbe4f1fe1a95512661de52c7f74
Francis <francispham89@gmail.com>
2018-04-18 17:06:26 -0700

    Done with Personal Information

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418234056) do
+ActiveRecord::Schema.define(version: 20180418234921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20180418234056) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "gender"
+    t.date "date_of_birth"
     t.index ["user_id"], name: "index_clients_on_user_id"
   end
 
