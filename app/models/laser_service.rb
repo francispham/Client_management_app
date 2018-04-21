1d592c095721eb1a569fd3522943602b817b56c0
Francis <francispham89@gmail.com>
2018-04-21 15:19:40 -0700

    Deleted LaserService and Edited seed.rb

--- app/models/laser_service.rb
+++
@@ -1,6 +0,0 @@
-class LaserService < ApplicationRecord
-  belongs_to :user
-  belongs_to :client
-
-  validates :health_condition, presence: true
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 PASSWORD = 'supersecret'
 
 User.destroy_all
-Category.destroy_all
-LaserService.destroy_all
 Client.destroy_all
 
 super_user = User.create(
@@ -12,8 +10,6 @@ super_user = User.create(
   password: PASSWORD,
   is_admin: true
 )
-
-
 
 3.times.each do
   first_name = Faker::Name.first_name
@@ -29,26 +25,12 @@ end
 
 users = User.all
 
-10.times.each do
-  Category.create(
-    name: Faker::Team.state
-    # ['Laser hair removal', 'Microdermabrasion', 'Acne treatment',
-    #        'Skin rejuvenation/Photorejuvnation',
-    #        'Fillers (Juverderm/Restylane/Perlane)',
-    #        'Varicose vein/Spider vein treatment',
-    #        'Skin care advise/Products', 'Liver spots/Age spots',
-    #        'Facial vein treatements', 'Botox', 'Rosacea', 'Thermage',
-    #        'Body contouring']
-  )
-end
-
-categories = Category.all
 
 10.times.each do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
 
-  c = Client.create(
+  Client.create(
     first_name: first_name,
     last_name: last_name,
     email: "#{first_name.downcase}.#{last_name.downcase}@example.com",
@@ -58,27 +40,58 @@ categories = Category.all
     postal_code:Faker::Address.postcode,
     user: users.sample
   )
-  if c.valid?
-    rand(0..2).times.each do
-      LaserService.create(
-        title: Faker::Educator.campus,
-        health_condition: Faker::Seinfeld.quote,
-        client: c,
-        user: users.sample
-      )
-    end
-  end
-  c.categories = categories.shuffle.slice(0..rand(3))
 end
 
 clients = Client.all
-laser_services = LaserService.all
 
 puts Cowsay.say "Created #{users.count} users", :tux
-puts Cowsay.say "Created #{categories.count} categories", :tux
 puts Cowsay.say "Created #{clients.count} clients", :kitty
-puts Cowsay.say "Created #{laser_services.count} laser_services", :sheep
 
 puts "Login as admin with #{super_user.email} and password of '#{PASSWORD}'"
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
-MedicalHistory.create([{name: 'Acne'}, {name: 'Arthiritis'}, {name: 'Autoimmunte'},{name: 'Blood disorders'}])
+MedicalHistory.create([
+  {name: 'Acne'}, {name: 'Arthiritis'}, {name: 'Autoimmune disorder (eg Lupus)'},
+  {name: 'Blood disorders'}, {name: 'Cancer (or radiation therapy)'},
+  {name: 'Diabetes/ Diabetic neuropathy'}, {name: 'Herpes (cold sores)'},
+  {name: 'Neurological disease'}, {name: 'Bruise easily'},{name: 'Hepatitis A'},
+  {name: 'Hepatitis A'}, {name: 'Hepatitis B'},{name: 'Hepatitis C'},
+  {name: 'Hirsutism (excess body hair)'}, {name: 'Vitiligo (white skin patches)'},
+  {name: 'Kidney disease'}, {name: 'Melanoma'}, {name: 'Port wine stain'},
+  {name: 'Psoriasis'}, {name: 'Pacemaker'}, {name: 'Liver disease'}, {name: 'HIV'},
+  {name: 'Migraine headaches'}, {name: 'Epilepsy/Seizures'}, {name: 'Shingles'},
+  {name: 'Skin pigmentation'}, {name: 'Steroid or hormanal therapy'},
+  {name: 'Hormonal imbalances'}, {name: 'Polycystic ovary syndrome'},
+  {name: 'Keloid scars/Other scars'}, {name: 'History of stroke'},
+  {name: 'Heart disease'}, {name: 'High blood pressure'}, {name: 'Thyroid condition'},
+  {name: 'Rosacea'}])
+Category.create([
+  {name: 'Liver sports/Age spots'}, {name: 'Facial vein treatments'}, {name: 'Botox'},
+  {name: 'Organic Teeth whitening'}, {name: 'Aqua Hydra-facial'},
+  {name: 'Mesotherapy/Microneedling'}, {name: 'Deep Facial Cleanse Treatments'},
+  {name: 'Laser Treatments'}, {name: 'Dermal Injectables'}, {name: 'Elapromed Stemcell'},
+  {name: 'Laser hair removal'}, {name: 'Microdermabrasion'}, {name: 'Acne Treatment'},
+  {name: 'Skin rejuvenation/Photorejuvenation'}, {name: 'Fillers (Juvederm/Restylan/Perlane) and Other'},
+  {name: 'Varicose vein/Spider vein treatment'}, {name: 'Skin care advice/Products'},
+  {name: 'Rosacea'}, {name: 'Thermage'}, {name: 'Body contouring'}, {name: 'Hepatitic'}
+  ])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
