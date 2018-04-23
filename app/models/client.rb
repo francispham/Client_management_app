14738a399332582d54122b7da64f85da6e84ff67
Francis <francispham89@gmail.com>
2018-04-23 14:27:46 -0700

    Fixed client validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -9,13 +9,12 @@ class Client < ApplicationRecord
   has_many :services, dependent: :destroy
   has_many :service_records, dependent: :nullify
 
-  validates :first_name, :last_name, :address, :postal_code, presence: true
-
+  validates :first_name, :last_name, presence: true
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
   validates :email,
-            presence: true,
+            # presence: true,
             uniqueness: true,
             format: VALID_EMAIL_REGEX
 
