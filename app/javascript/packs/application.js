32264b8f3891c1f741d225acd348a2598e375c03
Francis <francispham89@gmail.com>
2018-05-02 15:37:50 -0700

    Install Rails-React and Working with AdminIndex

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,12 +6,27 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
+import ReactDOM from "react-dom";
+import React from "react";
 
-import {qS} from "../src/dom";
+import {qS, qSA} from "../src/dom";
+import UserTable from "../components/UserTable";
 
 console.log('Hello World from Webpacker')
 
 document.addEventListener("DOMContentLoaded", () =>{
   const welcomeTitle = document.querySelector(".welcome-title");
   welcomeTitle.style.color = "DarkBlue";
+  if (welcomeTitle) {
+    welcomeTitle.style.color = "darkskyblue";
+  }
+
+  // RactDOM.render(
+  //   <UserTable/>,
+  //   qS("#user-table")
+  // );
 })
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true)
+var ReactRailsUJS = require("react_ujs")
+ReactRailsUJS.useContext(componentRequireContext)
