6de2453e4c45f8f213baaa983936292c18df61fc
Francis <francispham89@gmail.com>
2018-07-04 10:13:02 -0700

    Add images

---
+++ app/javascript/components/Services.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Serivces extends Component {
+  render () {
+    return (
+      <main className="Serivces d-flex justify-content-center" style={{padding:"10em"}}>
+        <h3 className="display-3">Services</h3>
+      </main>
+    )
+  }
+}
+
+export { Serivces };
