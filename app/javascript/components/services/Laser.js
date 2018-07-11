03d233487c0882f75a562290685bc823778b9712
Francis <francispham89@gmail.com>
2018-07-11 00:57:54 -0700

    Edit Laser 1

--- app/javascript/components/services/Laser.js
+++ app/javascript/components/services/Laser.js
@@ -7,6 +7,37 @@ class Laser extends Component {
     return (
       <main className="Laser middle flex-column" style={{paddingTop:"10em"}}>
         <h4 className="display-4">LASER HAIR REMOVAL</h4>
+        <div className="container-fluid">
+          <Container fluid style={{ paddingTop: "2em" }} >
+            <Row>
+              <Col className="middle" sm={12} md={6} style={{ padding:"0px" }}>
+                <img src={require('../../../assets/images/HairRemoval.png')}
+                    style={{ width: "50%" }}/>
+              </Col>
+              <Col sm={12} md={6} style={{ padding:"0px" }}>
+                <h4>Small Treatment Area</h4>
+                  <p>
+                    For one of the following areas: areola, lip, chin, cheeks,
+                    nose, nape/neck, sideburns, navel line.
+                  </p><br/>
+                <h4>Medium Treatment Area</h4>
+                  <p>
+                    For one of the following areas: bikini, underarms, full face,
+                    hands, feet.
+                  </p><br/>
+                <h4>Large Treatment Area</h4>
+                  <p>
+                    For one of the following areas: shoulder, brazilian bikini,
+                    chest, abdomen, upper back or lower back, arms (elbow up) or
+                    (elbow down), leg (knee up) or (knee down)
+                  </p><br/>
+                <br/>
+                <br/>
+                <p>Contact us to schedule your appointment today! </p>
+              </Col>
+            </Row>
+          </Container>
+        </div>
 
         <section className="container-fluid color">
           <Container fluid id="information" style={{
